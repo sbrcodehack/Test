713d52dc24c0055e5d62c3125738ac75bf4088a6
sbrcodehack <50989492+sbrcodehack@users.noreply.github.com>
2025-08-27 19:51:48 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'bouncing-ball-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  // You can add other assets here if they exist, like images or fonts
+];
+
+self.addEventListener('install', (event) => {
+  // Perform installation steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
